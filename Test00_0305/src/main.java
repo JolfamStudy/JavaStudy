58d7195582dae7bccc229d74e99e553437c6f6a7
jieun <kjeun0105@gmail.com>
2017-03-05 17:22:02 +0900

    #change

--- Test00_0305/src/main.java
+++ Test00_0305/src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("jolfam01");
-		System.out.println("jolfam02");
+		System.out.println("jolfam32");
 		System.out.println("jolfam03");
 	}
 
