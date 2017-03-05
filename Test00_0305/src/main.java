1c704e81c4b32a942a3f9fd352fc0c2763c97439
JongGyun <jonggyuni@gmail.com>
2017-03-06 00:18:35 +0900

    #Test Commit for New branch

--- Test00_0305/src/main.java
+++ Test00_0305/src/main.java
@@ -2,9 +2,7 @@
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("jolfam01");
-		System.out.println("jolfam32");
-		System.out.println("jolfam03");
+		System.out.println("JongGyun's Euler Practice");
 	}
 
 }
